4be33eaaae8fd6e0a917250659697d4919e23f8e
Jeff Verkoeyen <jverkoey@gmail.com>
2015-03-09 15:32:59 -0400

    Update source license.

--- src/NimbusKitBasics.h
+++ src/NimbusKitBasics.h
@@ -1,9 +1,26 @@
 /*
- Copyright (c) 2011-present, NimbusKit. All rights reserved.
+This is free and unencumbered software released into the public domain.
 
- This source code is licensed under the BSD-style license found in the LICENSE file in the root
- directory of this source tree and at the http://nimbuskit.info/license url. An additional grant of
- patent rights can be found in the PATENTS file in the same directory and url.
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
  */
 
 #import <Foundation/Foundation.h>
